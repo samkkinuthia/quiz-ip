d8827e31ee689c52fb3ae78b64dd60c32223c7ba
Samuel Kinuthia <samkkinuthia28@gmail.com>
2019-04-08 15:19:06 +0300

    added script tag for javascript

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
-    $("#questions").submit(function(event){
-        event.preventDefault()
-        $("#results").text(results)
-        var question1 =parseInt($("input:radio[name=question-1]:checked").val());
+    $("#questions").submit 
+        $("#results").text(answer)
+        var question1 =parseInt($("input:radio[name=question1]:checked").val());
+        var question2 =parseInt($("input:radio[name=question2]:checked").val());
+        var question3 =parseInt($("input:radio[name=question3]:checked").val());
+        var question4 =parseInt($("input:radio[name=question4]:checked").val());
+        var question5 =parseInt($("input:radio[name=question5]:checked").val());
+
+
+
+        var answer = question1 + question2 + question3 + question4 + question5
+
+        if(answer>80){
+        $("#results").text ("GREAT JOB!"+answer)
+        }
+        else if (answer>=60){
+            $("#results").text("Above average. Good try" + answer)
+
+        }
+        else{
+            $("#results").text("Below average. please try again" + answer)
+        }
+    
+
+
 
     })
-})
